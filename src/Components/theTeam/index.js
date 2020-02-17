3dbf9190e1524cc413b84030c48c474877b0fb9c
Jovan Derebanoski <jd7@students.kiron.ngo>
2020-02-17 12:30:42 +0100

    the_teams : Component & Pictures added

--- src/Components/theTeam/index.js
+++ src/Components/theTeam/index.js
@@ -32,13 +32,14 @@ class TheTeam extends Component {
                         .then( url => {
                             players[key].url = url; // here we add a new property URL where the images will be
                             resolve(); // after all promises are going to be resolved then we are going to set the state
-                            console.log(url);
-                            
+                         
+                            // console.log(url)
                         })
                     })
                 );
             }
 
+            // we want to proceed forward when all promises are resolved in this case thte images
             Promise.all(promises).then(()=>{
                 this.setState({
                     loading: false,
@@ -47,15 +48,72 @@ class TheTeam extends Component {
             });
         });
     }
+    showplayersByCategory = (category) => (
+        this.state.players 
+        ?
+            this.state.players.map((player, i ) => {
+                return player.position === category ?
+                // on each itteration we are going to have a differetn number
+                    <Fade left delay={i*40} key={i}>
+                        <div className="item">
+                            <PlayerCard 
+                                number={player.number}
+                                name={player.name}
+                                lastname={player.lastname}
+                                bck={player.url}
+                            />
+                        </div>
+                    </Fade>
+                    : null
+            })
+        :
+        null
+    )
 
     render() {
-        console.log(this.state.players);
+        // console.log(this.state.players);
         
-        return (
-            <div>
-                
+        return <div className="the_team_container" 
+                    style={{ background : `url(${Stripes}) repeat`}}>
+
+                    { 
+                    !this.state.loading ? 
+                       <div>
+
+                            <div className="team_category_wrapper">
+                                    <div className="title">Keeperes</div>
+                                    <div className="team_cards">
+                                        {this.showplayersByCategory('Keeper')}
+                                    </div> 
+                            </div>
+
+                            <div className="team_category_wrapper">
+                                    <div className="title">Defence</div>
+                                    <div className="team_cards">
+                                        {this.showplayersByCategory('Defence')}
+                                    </div> 
+                            </div>
+
+                            <div className="team_category_wrapper">
+                                    <div className="title">Midfield</div>
+                                    <div className="team_cards">
+                                        {this.showplayersByCategory('Midfield')}
+                                    </div> 
+                            </div>
+
+                            <div className="team_category_wrapper">
+                                    <div className="title">Strikers</div>
+                                    <div className="team_cards">
+                                        {this.showplayersByCategory('Striker')}
+                                    </div> 
+                            </div>
+                            
+                       </div>
+                    :null
+                    }   
+
             </div>
-        )
+        
     }
 }
 
