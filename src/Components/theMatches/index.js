e9547fb9f6b0d89b7a3a8641510c8ec82344c5ba
Jovan Derebanoski <jd7@students.kiron.ngo>
2020-02-17 23:21:50 +0100

    the_teams : Filtering results from the database

--- src/Components/theMatches/index.js
+++ src/Components/theMatches/index.js
@@ -13,7 +13,7 @@ class TheMatches extends Component {
         isLoading: true,
         matches: [],
         filterMatches: [],
-        playerFilter: 'All',
+        playedFilter: 'All',
         resultFilter: 'All'
     }
 
@@ -37,8 +37,19 @@ class TheMatches extends Component {
         });
         this.setState({
             filterMatches : played === 'All' ? this.state.matches : list,
-            playerFilter : played , 
+            playedFilter : played , 
             resultFilter : 'All'
+        })
+    }
+
+    showResult = (result) => {
+        const list = this.state.matches.filter((match) =>  {
+            return match.result === result
+        });
+        this.setState({
+            filterMatches : result === 'All' ? this.state.matches : list,
+            playedFilter  : 'All' , 
+            resultFilter  : result
         })
     }
 
@@ -56,7 +67,7 @@ class TheMatches extends Component {
                                     Show Match
                                 </div>
                                 <div className="cont">
-                                    <div className={`option`}
+                                    <div className={`option ${state.playedFilter === 'All' ? 'active' : ''}`}
                                         onClick={() => this.showPlayed('All')}
                                         >
                                         All
@@ -70,6 +81,33 @@ class TheMatches extends Component {
                                      onClick={() => this.showPlayed('No')}
                                     >
                                         Not Played
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="match_filters_box">
+                                <div className="tag">
+                                    Result game
+                                </div>
+                                <div className="cont">
+                                    <div className={`option ${state.resultFilter === 'All' ? 'active' : ''}`}
+                                        onClick={() => this.showResult('All')}
+                                        >
+                                        All
+                                    </div>
+                                    <div className={`option ${state.resultFilter === 'W' ? 'active' : ''}`}
+                                        onClick={() => this.showResult('W')}
+                                        >
+                                        W
+                                    </div>
+                                    <div className={`option ${state.resultFilter === 'L' ? 'active' : ''}`}
+                                     onClick={() => this.showResult('L')}
+                                    >
+                                        L
+                                    </div>
+                                    <div className={`option ${state.resultFilter === 'D' ? 'active' : ''}`}
+                                     onClick={() => this.showResult('D')}
+                                    >
+                                        D
                                     </div>
                                 </div>
                             </div>
