357790ae0644474c16a608018ca23aa3a9e6224d
Jovan Derebanoski <jd7@students.kiron.ngo>
2020-02-17 23:11:25 +0100

    the_teams : Filtering the data we are going to get from the database

--- src/Components/theMatches/MatchesList.js
+++ src/Components/theMatches/MatchesList.js
@@ -1,80 +1,105 @@
 import React, { Component } from 'react'
-import { easePolyOut }      from 'd3-ease';
-import NodeGroup            from 'react-move/NodeGroup';
+import { easePolyOut } from 'd3-ease';
+import NodeGroup from 'react-move/NodeGroup';
 
 class MatchesList extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             matchesList : []
+            matchesList: []
         }
     }
 
-    static getDerivedStateFromProps(props, state)  {
+    static getDerivedStateFromProps(props, state) {
         //this will update the state
         return state = {
-            matchesList : props.matches
+            matchesList: props.matches
         }
     }
 
     showMatches = () => (
-        this.state.matchesList 
-            ? 
-                <NodeGroup
-                    data={this.state.matchesList}
-                    keyAccessor={(d) => d.id}
+        this.state.matchesList
+            ?
+            <NodeGroup
+                data={this.state.matchesList}
+                keyAccessor={(d) => d.id}
 
-                    start={() => ({
-                        opacity : 0,
-                        x : -200,
+                start={() => ({
+                    opacity: 0,
+                    x: -200,
 
-                    })}
+                })}
 
-                    enter={( d, i ) => ({
-                        opacity : [1],
-                        x       : [0],
-                        timing  : { duration : 500, delay : i * 50, ease : easePolyOut}
-                    })}
+                enter={(d, i) => ({
+                    opacity: [1],
+                    x: [0],
+                    timing: { duration: 500, delay: i * 50, ease: easePolyOut }
+                })}
 
-                    update={( d, i ) => ({
-                        opacity : [1],
-                        x       : [0],
-                        timing  : { duration : 500, delay : i * 50, ease : easePolyOut}
-                    })}
+                update={(d, i) => ({
+                    opacity: [1],
+                    x: [0],
+                    timing: { duration: 500, delay: i * 50, ease: easePolyOut }
+                })}
 
-                    leave={( d, i ) => ({
-                        opacity : [0],
-                        x       : [-200],
-                        timing  : { duration : 500, delay : i * 50, ease : easePolyOut}
-                    })}
+                leave={(d, i) => ({
+                    opacity: [0],
+                    x: [-200],
+                    timing: { duration: 500, delay: i * 50, ease: easePolyOut }
+                })}
 
-                >
+            >
                 {
                     (nodes => (
                         <div>
-                            { nodes.map(({ key, data, state : { x, opacity }}) => (
+                            {nodes.map(({ key, data, state: { x, opacity } }) => (
                                 <div
                                     key={key} className="match_box_big"
                                     style={{
                                         opacity,
                                         transform: `translate(${x}px)`
-                                    }}     
+                                    }}
                                 >
+                                    <div className="block_wraper">
+                                        <div className="block">
+                                            <div
+                                                className="icon"
+                                                style={{ background: `url(/images/team_icons/${data.localThmb}.png)` }}>
+                                            </div>
+                                            <div className="team">{data.local}</div>
+                                            <div className="result">{data.resultLocal}</div>
+                                        </div>
+                                        <div className="block">
+                                            <div
+                                                className="icon"
+                                                style={{ background: `url(/images/team_icons/${data.awayThmb}.png)` }}>
+                                            </div>
+                                            <div className="team">{data.away}</div>
+                                            <div className="result">{data.resultAway}</div>
+                                        </div>
+
+                                    </div>
+                                    <div className="block_wraper nfo">
+                                        <div> <strong>Date : </strong>{data.date} </div>
+                                        <div> <strong>Stadium : </strong>{data.stadium} </div>
+                                        <div> <strong>Referee : </strong>{data.referee} </div>
+                                    </div>
 
                                 </div>
+
                             ))}
                         </div>
                     ))
                 }
-         </NodeGroup>
+            </NodeGroup>
             :
-        null
+            null
     )
-    
+
     render() {
         // console.log(this.props);
-        
+
         return (
             <div>
                 {this.showMatches()}
--- src/Components/theMatches/index.js
+++ src/Components/theMatches/index.js
@@ -10,45 +10,77 @@ import LeagueTable from './table'
 class TheMatches extends Component {
 
     state = {
-        isLoading       : true,
-        matches         : [],
-        filterMatches   : [],
-        playerFilter    : 'All',
-        resultFilter    : 'All'
+        isLoading: true,
+        matches: [],
+        filterMatches: [],
+        playerFilter: 'All',
+        resultFilter: 'All'
     }
 
     componentDidMount() {
         // fetching the data from the server database
         firebaseMatches.once('value')
-        .then(snapshot => {
-            const matches = firebaseLooper(snapshot);
+            .then(snapshot => {
+                const matches = firebaseLooper(snapshot);
 
-            this.setState({ 
-                isLoading : false,
-                matches: reverseArray(matches),
-                filterMatches : reverseArray(matches)
+                this.setState({
+                    isLoading: false,
+                    matches: reverseArray(matches),
+                    filterMatches: reverseArray(matches)
+                })
             })
+    }
+
+    showPlayed = (played) => {
+        const list = this.state.matches.filter((match) =>  {
+            return match.final === played
+        });
+        this.setState({
+            filterMatches : played === 'All' ? this.state.matches : list,
+            playerFilter : played , 
+            resultFilter : 'All'
         })
     }
 
     render() {
         // console.log(this.state);
         const state = this.state
-        
+
         return (
             <div className="the_matches_container">
-               <div className="the_matches_wrapper">
+                <div className="the_matches_wrapper">
                     <div className="left">
                         <div className="match_filters">
-
+                            <div className="match_filters_box">
+                                <div className="tag">
+                                    Show Match
+                                </div>
+                                <div className="cont">
+                                    <div className={`option`}
+                                        onClick={() => this.showPlayed('All')}
+                                        >
+                                        All
+                                    </div>
+                                    <div className={`option`}
+                                        onClick={() => this.showPlayed('Yes')}
+                                        >
+                                        Played
+                                    </div>
+                                    <div className={`option`}
+                                     onClick={() => this.showPlayed('No')}
+                                    >
+                                        Not Played
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <MatchesList matches={state.filterMatches} />
-                      
+
                     </div>
                     <div className="right">
                         <LeagueTable />
                     </div>
-               </div>
+                </div>
             </div>
         )
     }
